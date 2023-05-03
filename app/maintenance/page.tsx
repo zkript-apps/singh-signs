ad74ae186c17d7d39b1b4f95626d723a1ac29525
Victor Higoy <higoyvictor143@gmail.com>
2023-05-03 10:14:38 +0800

    added maintenance section

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/Header";
+import Content from "@/components/pages/maintenance/Content";
+import { metaBuilder } from "@/helpers/metaBuilder";
+
+export async function generateMetadata() {
+  return metaBuilder({
+    title: "Maintenance - Fremont Sign Company",
+    description:
+      "The exterior signs serve an incredibly important role in your business. With an eye-popping signage, you are sure that you are making your",
+  });
+}
+
+export default function Maintenance() {
+  return (
+    <>
+      <Header title="Maintenance" root="Home" />
+      <Content />
+    </>
+  );
+}
